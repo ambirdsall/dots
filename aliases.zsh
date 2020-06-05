8d8b700b6d010bf7a2c02053f95146b93b46561e
Alex Birdsall <alex.birdsall@indiegogo.com>
2020-06-04 23:13:32 -0700

    Fix up some fuzzy finding aliases

--- aliases.zsh
+++ aliases.zsh
@@ -79,25 +79,28 @@ alias sane='stty sane'
 
 # * first you have to be able to get where you're going
 # ** jump around
-# DEPENDENCY: gem install rouge
+
 alias um="find . -type f | grep -vE '.tmp-*|.git|node_modules|bower_components' | fzf --multi --preview 'bat --color=always {}'"
 cdf() {
-  pushd $(dirname $(find . -type f | grep -vE '.tmp-*|.git|node_modules|bower_components|DS_Store' | fzf --preview 'rougify {}'))
+  pushd $(dirname $(find . -type f | grep -vE '.tmp-*|.git|node_modules|bower_components|DS_Store' | fzf --preview 'bat --color=always {}'))
 }
-# h () {
-#   local cols sep
-#   cols=$(( COLUMNS / 3 ))
-#   sep='{::}'
+# * fuzzy find from chrome history and open url (mac-specific for now)
+if [ $(uname) == 'Darwin' ]; then
+    h () {
+        local cols sep
+        cols=$(( COLUMNS / 3 ))
+        sep='{::}'
 
-# TODO: find chrome history's location on linux
-#   cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+        # TODO: find chrome history's location on linux
+        cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
 
-#   sqlite3 -separator $sep /tmp/h \
-#     "select substr(title, 1, $cols), url
-#      from urls order by last_visit_time desc" |
-#   awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-#   fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
-# }
+        sqlite3 -separator $sep /tmp/h \
+                "select substr(title, 1, $cols), url
+     from urls order by last_visit_time desc" |
+            awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+            fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+    }
+fi
 
 
 
