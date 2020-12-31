15b660d81c2513b3f20f5c1b71ae2ce2a878011d
Alex Birdsall <ambirdsall@gmail.com>
2020-12-31 15:58:17 -0500

    Improve fonts, esp for linux

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,9 +21,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font "Fira Code")
+(setq doom-font "FiraCode")
 ;; ;; why doesn't the sizing work?!?!?!?!?!?!?!?
-(setq doom-variable-pitch-font "Baskerville-18")
+(setq doom-variable-pitch-font (if IS-MAC "Baskerville-18" "LibreBaskerville"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
