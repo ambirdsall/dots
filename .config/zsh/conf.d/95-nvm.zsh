ba3a1f7f9962a7ed35b0f770f29b2586ff3902a6
Alex Birdsall <ambirdsall@gmail.com>
2023-04-04 13:46:48 -0700

    add .config/zsh/conf.d for cleaner, more modular config

---
+++ .config/zsh/conf.d/95-nvm.zsh
@@ -0,0 +1,30 @@
+check_node_version() {
+	# When a .nvmrc file in the current directory exists ..
+	if [[ "$IGNORE_NODE_VERSION" != "1" && -f ".nvmrc" ]]; then
+		nvmrc=`cat .nvmrc`
+		# .. check whether the current node version is the required one
+		if [ ! -x "$(command -v node)" ] || [ `node -v` != `nvm version $nvmrc` ]; then
+			# If not use nvm to set the version to the one from .node-version
+			echo -e "$fg_bold[yellow][.nvmrc] Switching to $nvmrc$reset_color"
+			nvm ls "$nvmrc" &> /dev/null
+			if [ $? -eq 0 ]; then
+				nvm use "$nvmrc"
+			else
+				nvm install "$nvmrc"
+			fi
+			if [ $? -eq 0 ]; then
+				echo -e "$fg_bold[green]Done!$reset_color"
+			else
+				echo -e "$fg_bold[red]Failed!$reset_color"
+			fi
+		fi
+	fi
+}
+
+if [ -f "/usr/share/nvm/init-nvm.sh" ]; then
+	source /usr/share/nvm/init-nvm.sh
+	add-zsh-hook precmd check_node_version
+elif [ -f "$HOME/.nvm/nvm.sh" ]; then
+	source "$HOME/.nvm/nvm.sh"
+	add-zsh-hook precmd check_node_version
+fi
