13c8eb81db9dc6b7828e8eaa09caf58cda9c328e
Alex Birdsall <ambirdsall@gmail.com>
2023-09-08 11:17:48 -0700

    Fix grimshot path in script

--- .config/sway/scripts/screenshot.sh
+++ .config/sway/scripts/screenshot.sh
@@ -6,13 +6,13 @@ selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widget
  
 case $selected in
   active)
-    /usr/share/sway/scripts/grimshot --notify save active;;
+    grimshot --notify save active;;
   screen)
-    /usr/share/sway/scripts/grimshot --notify save screen;;
+    grimshot --notify save screen;;
   output)
-    /usr/share/sway/scripts/grimshot --notify save output;;
+    grimshot --notify save output;;
   area)
-    /usr/share/sway/scripts/grimshot --notify save area;;
+    grimshot --notify save area;;
   window)
-    /usr/share/sway/scripts/grimshot --notify save window;;
+    grimshot --notify save window;;
 esac
