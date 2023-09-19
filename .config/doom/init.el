08f5edff5fe73ecfaf11301d6513b2ed3d32a3d7
Alex Birdsall <ambirdsall@gmail.com>
2023-09-19 08:55:17 -0700

    Add python to config

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -176,7 +176,10 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       (python
+        +lsp
+        +tree-sitter
+        +pyenv)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
