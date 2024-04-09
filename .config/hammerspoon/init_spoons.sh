dd8b3017a4a1e547102f1156569fea15877a8c83
Alex Birdsall <ambirdsall@gmail.com>
2024-04-09 11:31:19 -0700

    Add script to clone and configure all spoons

---
+++ .config/hammerspoon/init_spoons.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Script to clone spoon repositories and define the necessary remotes for contributing
+hammer="$HOME/.hammerspoon"
+
+if [ ! -d "$hammer" ]; then
+    ln -s "$HOME/.config/hammerspoon" "$hammer"
+fi
+
+mkdir -p $hammer/Spoons
+
+git clone  $hammer/Spoons/ModalMgr.spoon
+
+git clone git@github.com:ambirdsall/PaperWM.spoon.git $hammer/Spoons/PaperWM.spoon
+git remote add origin git@github.com:ambirdsall/PaperWM.spoon.git
+git remote add origin git@github.com:ambirdsall/PaperWM.spoon.git
+git remote add upstream https://github.com/mogenson/PaperWM.spoon
+git remote add upstream https://github.com/mogenson/PaperWM.spoon
+
+git clone git@github.com:ambirdsall/spacehammer.git $hammer/Spoons/Spacehammer.spoon
+git remote add agzam git@github.com:agzam/spacehammer.git
+git remote add agzam git@github.com:agzam/spacehammer.git
+git remote add jaide https://github.com/jaidetree/spacehammer.git
+git remote add jaide https://github.com/jaidetree/spacehammer.git
+git remote add origin git@github.com:ambirdsall/spacehammer.git
+git remote add origin git@github.com:ambirdsall/spacehammer.git
+
