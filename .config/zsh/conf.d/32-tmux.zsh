854b2eafa222087bff5f52c9840436dd6d589f88
Alex Birdsall <ambirdsall@gmail.com>
2024-04-29 14:12:08 -0700

    Oh hey, fzf-tmux

--- .config/zsh/conf.d/32-tmux.zsh
+++ .config/zsh/conf.d/32-tmux.zsh
@@ -10,10 +10,17 @@ list_sessions_if_inside_tmux() {
 
 alias clear='clear; [[ -z "$TMUX" ]] && tls 2>/dev/null || true'
 
-alias t=tmux
-alias ta="tmux attach -t"
-alias tt="tmux attach -t"
-alias tk="tmux kill-session -t"
+if command -v fzf-tmux >/dev/null; then
+    alias t=fzf-tmux
+    alias tt="fzf-tmux attach -t"
+    alias tk="fzf-tmux kill-session -t"
+else
+    alias t=tmux
+    alias ta="tmux attach -t"
+    alias tt="tmux attach -t"
+    alias tk="tmux kill-session -t"
+fi
+
 tn () {
     if [[ $# -gt 0 ]]; then
         tmux new -s $1; cd; clear
