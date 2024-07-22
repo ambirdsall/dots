88ff837d0c30d29da65533f54676a3b57adf3296
Alex Birdsall <ambirdsall@gmail.com>
2024-07-22 15:10:47 -0700

    Make pbcopy/pbpaste dumber aliases for scripts
    This plays better with noninteractive use cases.

--- .config/zsh/conf.d/24-copy-paste.zsh
+++ .config/zsh/conf.d/24-copy-paste.zsh
@@ -2,11 +2,6 @@
 
 # it's just a better clipboard API, tbh; plus muscle memory
 if [ $(uname) != 'Darwin' ]; then
-  if command -v wl-copy &>/dev/null; then
-    alias pbcopy='wl-copy'
-    alias pbpaste='wl-paste --type text'
-  elif command -v xclip &>/dev/null; then
-    alias pbcopy='xclip -i -selection clipboard'
-    alias pbpaste='xclip -o -selection clipboard'
-  fi
+  alias pbcopy='clipboard-copy'
+  alias pbpaste='clipboard-paste'
 fi
