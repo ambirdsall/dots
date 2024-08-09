526c98e29cebbe868cda498c78f7b22e26016644
Alex Birdsall <ambirdsall@gmail.com>
2024-08-09 00:03:51 -0700

    Which `which` is which

--- .config/zsh/conf.d/12-path-utils.zsh
+++ .config/zsh/conf.d/12-path-utils.zsh
@@ -1,4 +1,8 @@
-dots/clear_name which
-which () {
-  (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
-}
+# mac uses BSD which, which acts nicely enough on its own and uses different
+# options than the GNU coreutils one
+if [ $IS_MAC != t ]; then
+	dots/clear_name which
+	which () {
+	  (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
+	}
+fi
