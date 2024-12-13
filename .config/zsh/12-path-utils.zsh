70f98fe09d7bf05898dec9421742e57f0288f189
Alex Birdsall <ambirdsall@gmail.com>
2024-12-13 15:04:36 -0800

    Fix linux `which` wrapper and its guard condition

--- .config/zsh/12-path-utils.zsh
+++ .config/zsh/12-path-utils.zsh
@@ -1,8 +1,8 @@
 # mac uses BSD which, which acts nicely enough on its own and uses different
 # options than the GNU coreutils one
-if [ $IS_MAC != t ]; then
+if ! [ $IS_MAC ]; then
 	dots/clear_name which
 	which () {
-	  (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
+	  (alias; declare -f) | command which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
 	}
 fi
