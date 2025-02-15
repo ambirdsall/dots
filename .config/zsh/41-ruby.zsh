2c8bdc4a61e48b851ce8e63fb99cd029b7de45d9
Alex Birdsall <ambirdsall@gmail.com>
2025-02-15 13:39:18 -0800

    Quiet successful rbenv checks

--- .config/zsh/41-ruby.zsh
+++ .config/zsh/41-ruby.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-if command -v rbenv; then
+if dots/at_hand rbenv; then
     [ $IS_MAC ] && export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
     eval "$(rbenv init -)"
 fi
