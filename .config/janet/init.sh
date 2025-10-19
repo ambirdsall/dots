0d93050e7511c722bfad8add78155e469243aabe
Alex Birdsall <ambirdsall@gmail.com>
2025-10-19 16:40:48 -0700

    Tweak janet installation script

--- .config/janet/init.sh
+++ .config/janet/init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
 if [ ! -d "$HOME/c/janet" ]; then
-    cd ~/c
+    cd ~/c || exit 1
     git clone https://github.com/janet-lang/janet
-    cd janet || exit 1
-    make -j
-
-    # make test
-
-    sudo make install
-    sudo make install-jpm-git
-    sudo make install-spork-git
-
-    sudo jpm install sh
-    sudo jpm install cmd
 fi
+cd "$HOME/c/janet" || exit 1
+make -j
+
+# make test
+
+# TODO switch to non-root janet install
+sudo make install
+sudo make install-jpm-git
+sudo make install-spork-git
+
+sudo jpm install sh
