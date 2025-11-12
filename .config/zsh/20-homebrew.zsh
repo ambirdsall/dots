a13a9fd9f6c4464eecf1597647d67d0e73d87c12
Alex Birdsall <ambirdsall@gmail.com>
2025-11-12 10:52:09 -0800

    Very Important changes (whitespace/comment tweaks)

--- .config/zsh/20-homebrew.zsh
+++ .config/zsh/20-homebrew.zsh
@@ -1,6 +1,6 @@
 # This file gets such a low prefix number for two reasons:
 # 1) although homebrew is in nobody's POSIX spec, package management is pretty fundamental
-# 2) sourcing it after language-specific tooling (nv,rbenv,pyenv,asdf,etc) causes problems
+# 2) sourcing it after language-specific tooling (nv,rbenv,pyenv,asdf,etc) causes Problems
 
 if dots/at_hand /opt/homebrew/bin/brew; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
--- .config/zsh/31-fzf.zsh
+++ .config/zsh/31-fzf.zsh
@@ -1,6 +1,5 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-
 if dots/at_hand fzf; then
   alias um="find . -type f | grep -vE '\.tmp-*|\.git|node_modules|bower_components' | fzf --multi --preview 'bat --color=always {}'"
   cdf() {
