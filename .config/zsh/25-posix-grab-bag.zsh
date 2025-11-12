1f7321c9e6f30cba52381f6d29c3a5d0e2c73e0c
Alex Birdsall <ambirdsall@gmail.com>
2025-11-12 12:06:31 -0800

    Only alias `sl` if it's installed
    But, I mean, why would it not be installed

--- .config/zsh/25-posix-grab-bag.zsh
+++ .config/zsh/25-posix-grab-bag.zsh
@@ -6,8 +6,10 @@ alias ls="ls -GF"
 alias la="ls -A"
 alias ll="ls -lh"
 
-# allow exiting with C-c
-alias sl="sl -e"
+if dots/at_hand sl; then
+    # allow exiting with C-c
+    alias sl="sl -e"
+fi
 
 alias mkdir="mkdir -pv"
 
